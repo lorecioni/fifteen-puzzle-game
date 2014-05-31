cbf81d8ed1001b3c125fc170e13e30c64b92ca02
Lorenzo Cioni <lore.cioni@gmail.com>
2014-06-01 00:28:40 +0200

    fix quert

--- php/check-challenge-code.php
+++ php/check-challenge-code.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Methods: GET, POST');
 include("../../config.php");
 
 $code = $_GET['code'];
-$result = mysql_query("SELECT code FROM 15_challenges WHERE code='".$code."'");
+$result = mysql_query("SELECT code FROM 15_challenges WHERE code='".$code."' and player2_score = 0");
 
 if(mysql_num_rows($result) != 1){
   echo 'failure';
